9412fcac6b09b3e5aa1c3d6b33d3067d96198518
Javier Avelino <evilword.com@gmail.com>
2019-03-19 13:54:28 -0600

    Plantilla basica para el cotizador

---
+++ cotizador/src/componentes/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = propts => {
+    return (
+        <header className="top">
+            {propts.titulo} 
+        </header>
+
+        
+    )
+}
+
+export default Header;
